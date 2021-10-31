eca8cc6c7a4be38e1fa3f54617dd0255ebcac9a7
cfynes14 <70376968+cfynes14@users.noreply.github.com>
2021-10-31 08:41:57 +0000

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowerRouter as Router, Route, Switch  } from 'react-dom-router'
+
+//components
+import LoginBox from './components/Login/Login';
+import NewUser from './components/NewUser/NewUser';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Switch>
+        <Route path="/new_user">
+          <NewUser />
+        </Route>
+        <h1>Task Manager</h1>
+        <LoginBox />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Login/Login/Login.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import  { Link  }  from 'react-router-dom';
+import './login.scss'
+
+const LoginBox = () => {
+    return(
+        <div className="loginBox">
+            <h2 className="title">Login</h2>
+            <p className="newSignup">New user? <Link to="'/new_user">Sign up here</Link></p>
+            <form className="loginForm">
+                <label className="boxElement">Username:</label>
+                <input className=" boxElement" type="text boxElement"></input>
+                <label className="boxElement">Password:</label>
+                <input className="boxElement" type="password boxElement" />
+                <button className="boxElement loginButton" type="submit" onClick={e => e.preventDefault}>Login</button>
+                <button className="boxElement loginButton" onClick={e => e.preventDefault}>Cancel</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginBox;
