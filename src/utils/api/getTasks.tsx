e83da2cd4f01de851c2659b2b70308a221b9168b
Chris  Fynes <chris.fynes@fxdigital.uk>
2021-12-03 15:02:59 +0000

    add getTasks function

--- src/utils/api/getTasks.tsx
+++ src/utils/api/getTasks.tsx
@@ -1,3 +1,38 @@
-const getTasks = () => {};
+const getTasks = async () => {
+  console.log("getting tasks");
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    `Bearer ${window.sessionStorage.getItem("token")?.slice(1, -1)}`
+  );
+  myHeaders.append("Content-Type", "application/json");
+
+  //   var raw = JSON.stringify({
+  //     user: window.sessionStorage.getItem("user"),
+  //   });
+
+  var requestOptions: any = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  try {
+    const res = await fetch(
+      "https://fynes-task-manager.herokuapp.com/tasks",
+      requestOptions
+    );
+    const json = await res.json();
+    console.log(json);
+    return json;
+  } catch (e) {
+    console.log(e);
+  }
+
+  //   fetch("https://fynes-task-manager.herokuapp.com/tasks", requestOptions)
+  //     .then((response) => response.text())
+  //     .then((result) => console.log(result))
+  //     .catch((error) => console.log("error", error));
+};
 
 export default getTasks;
