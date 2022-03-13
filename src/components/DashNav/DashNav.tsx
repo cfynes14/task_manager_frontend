89842cc559c94a9958aa09d28240b76eef7390a6
Chris  Fynes <chris.fynes@fxdigital.uk>
2022-03-13 13:02:23 +0000

    create DashNav template

--- src/components/DashNav/DashNav.tsx
+++ src/components/DashNav/DashNav.tsx
@@ -1,18 +1,23 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { NavContainer } from "./styles";
+import { NavContainer, SecondaryNav } from "./styles";
 
 interface DashNavProps {
   openNewTaskModal: () => void;
+  dashboardHandleClick: (e: any) => void;
 }
 
 const DashNav = (props: DashNavProps) => {
-  const { openNewTaskModal } = props;
+  const { openNewTaskModal, dashboardHandleClick } = props;
   return (
     <>
-      <NavContainer logo="Task Manager" logoutText="Logout" />
-
-      {/* <SecondaryNav>
+      <NavContainer
+        logo="Task Manager"
+        logoutText="Logout"
+        dashboardHandleClick={dashboardHandleClick}
+      />
+      {/* 
+      <SecondaryNav>
         <a href="#">Logout</a>
       </SecondaryNav> */}
     </>
