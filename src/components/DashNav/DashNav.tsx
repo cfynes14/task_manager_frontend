8677f73320a2a0f0057e1a95bb4f9df1686f395b
Chris  Fynes <chris.fynes@fxdigital.uk>
2022-03-14 09:28:14 +0000

    refactor DashNav

--- src/components/DashNav/DashNav.tsx
+++ src/components/DashNav/DashNav.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 // import { Link } from "react-router-dom";
-import { NavContainer, SecondaryNav } from "./styles";
+
+import tw from "twin.macro";
+
+import LogoContainer from "./LogoContainer";
+import PrimaryNav from "./PrimaryNav";
+import SecondaryNav from "./SecondaryNav";
+import MobileNav from "./MobileNav";
 
 interface DashNavProps {
   openNewTaskModal: () => void;
@@ -9,17 +15,21 @@ interface DashNavProps {
 
 const DashNav = (props: DashNavProps) => {
   const { openNewTaskModal, dashboardHandleClick } = props;
+
   return (
     <>
-      <NavContainer
-        logo="Task Manager"
-        logoutText="Logout"
-        dashboardHandleClick={dashboardHandleClick}
-      />
-      {/* 
-      <SecondaryNav>
-        <a href="#">Logout</a>
-      </SecondaryNav> */}
+      <nav tw="bg-gray-100">
+        <div tw="max-w-5xl mx-auto">
+          <div tw="flex justify-between">
+            <div tw="flex space-x-4">
+              <LogoContainer logo={"Task Manager"} />
+              <PrimaryNav dashboardHandleClick={dashboardHandleClick} />
+            </div>
+            <SecondaryNav logoutText={"Logout"} />
+            <MobileNav dashboardHandleClick={dashboardHandleClick} />
+          </div>
+        </div>
+      </nav>
     </>
   );
 };
