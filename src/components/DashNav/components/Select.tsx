0dee5f328c56e8dd53c7abb3b7b577353ee91f45
Chris  Fynes <chris.fynes@fxdigital.uk>
2022-03-21 09:19:09 +0000

    add name to each select component

--- src/components/DashNav/components/Select.tsx
+++ src/components/DashNav/components/Select.tsx
@@ -3,16 +3,18 @@ import tw from "twin.macro";
 interface SelectInterface {
   options: string[];
   handleChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+  selectName: string;
 }
 
 const Select = (props: SelectInterface) => {
-  const { options, handleChange } = props;
+  const { options, handleChange, selectName } = props;
   // console.log("OPTIONS", options);
   return (
     <div>
       <select
         tw="mx-10 rounded"
         onChange={(e: React.ChangeEvent<HTMLSelectElement>) => handleChange(e)}
+        name={selectName}
       >
         {options.map((option) => (
           <option value={option}>{option}</option>
