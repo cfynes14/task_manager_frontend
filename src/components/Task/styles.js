1734ad4a275aacc621683c0abdc08ec16f6445b5
Chris  Fynes <chris.fynes@fxdigital.uk>
2022-04-25 19:49:33 +0100

    add responsive styles for task coordination on dashboard

--- src/components/Task/styles.js
+++ src/components/Task/styles.js
@@ -1,9 +1,12 @@
 import tw, { styled } from "twin.macro";
 
 const TaskStyles = styled.div`
-  ${tw`m-4 rounded border shadow`}
+  width: 30%%;
+
+  ${tw`sm:w-1/2 md:w-1/4 lg:w-2/12 m-3 rounded border shadow`}
+
   h3 {
-    ${tw`text-colour4 text-center bg-colour2 p-2 font-sans`}
+    ${tw`text-colour4 text-center bg-colour2 p-2 font-sans truncate`}
   }
   p {
     ${tw`block mt-2 text-center text-colour3`}
